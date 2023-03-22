852e7bd466fb5e2c98509f6bb07b61a4c55fc0a8
BakChanGyu <qmrma987@gmail.com>
2023-03-22 16:48:48 +0900

    삭제버튼 추가 기능ì은 직

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -9,11 +9,13 @@ import com.mysite.sbb.question.service.QuestionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.List;
@@ -64,5 +66,39 @@ public class QuestionController {
         Member member = memberService.getMember(principal.getName());
         questionService.create(questionForm.getSubject(), questionForm.getContent(), member);
         return "redirect:/sbb/question/list";
+    }
+
+    @GetMapping("/modify/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String modify(QuestionForm questionForm,
+                         @PathVariable long id,
+                         Principal principal) {
+        Question question = questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "수정권한이 없습니다.");
+        }
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+
+        return "question_form";
+    }
+
+    @PostMapping("/modify/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String questionModify(@Valid QuestionForm questionForm,
+                                 BindingResult bindingResult,
+                                 Principal principal,
+                                 @PathVariable("id") long id) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Question question = questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+
+        return String.format("redirect:/sbb/question/detail/%s", id);
     }
 }
